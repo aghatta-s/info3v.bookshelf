12ede93f860592e2820c82bea7c2a7a8f13c1567
Sophya <aghatta768@gmail.com>
2026-05-27 15:19:45 -0300

    testando as implementações

--- bookshelf/src/main/java/br/edu/ifrn/Main.java
+++ bookshelf/src/main/java/br/edu/ifrn/Main.java
@@ -1,7 +1,51 @@
 package br.edu.ifrn;
 
+import br.edu.ifrn.bookshelf.modelo.Livro;
+import br.edu.ifrn.bookshelf.servico.LivroServico;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        LivroServico service = new LivroServico();
+
+        Livro livro1 = new Livro();
+
+        livro1.setTitulo("Dom Casmurro");
+        livro1.setAutor("Machado de Assis");
+        livro1.setQuantidadeExemplares(5);
+        livro1.setExemplaresEmprestados(2);
+
+        System.out.println("---- Teste de Cadastro ----");
+
+        try {
+
+            service.cadastrarLivro(livro1);
+
+        } catch (Exception e) {
+
+            System.err.println("Erro ao cadastrar: " + e.getMessage());
+        }
+
+        System.out.println("\n---- Status do Livro ----");
+
+        System.out.println("Título: " + livro1.getTitulo());
+
+        System.out.println("Autor: " + livro1.getAutor());
+
+        System.out.println("Quantidade Total: "
+                + livro1.getQuantidadeExemplares());
+
+        System.out.println("Exemplares Disponíveis: "
+                + livro1.getExemplaresDisponiveis());
+
+        if (livro1.isIndisponivel()) {
+
+            System.out.println("Atenção: Livro indisponível!");
+
+        } else {
+
+            System.out.println("Status: Há exemplares disponíveis.");
+        }
     }
 }
